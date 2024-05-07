6491d7ed74a9013e7bdaf5848c056248546ab566
PeturDaniel <peturdaniel@live.com>
2024-05-07 12:24:33 +0000

    Added single job offer detail page
    Single job offer page, minor changes in index.html

--- job_offers/views.py
+++ job_offers/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from job_offers.models import JobOffer
 
 
 def index(request):
     context = {'job_offers': JobOffer.objects.all().order_by('title')}
     return render(request, 'job_offers_page/index.html', context)
+
+def get_job_offer_by_id(request, id):
+    return render(request, 'job_offers_page/job_offer_details.html', {
+        'job_offer': get_object_or_404(JobOffer, pk=id)
+    })
