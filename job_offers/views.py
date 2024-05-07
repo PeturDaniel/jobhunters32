bfdc50f4ba3025d9e5b6a2ab28869a519e9ffbeb
PeturDaniel <peturdaniel@live.com>
2024-05-07 14:17:23 +0000

    Minor syntax corrections

--- job_offers/views.py
+++ job_offers/views.py
@@ -7,9 +7,9 @@ def index(request):
     context = {'job_offers': JobOffer.objects.all().order_by('title')}
     return render(request, 'job_offers_page/index.html', context)
 
+
 @login_required
 def get_job_offer_by_id(request, id):
     return render(request, 'job_offers_page/job_offer_details.html', {
         'job_offer': get_object_or_404(JobOffer, pk=id)
     })
-
