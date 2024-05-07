ae861c93c9105d239f47d81cb0795cc318f35af3
PeturDaniel <peturdaniel@live.com>
2024-05-07 10:48:52 +0000

    Updated job offer model

--- job_offers/models.py
+++ job_offers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class JobOffer(models.Model):
@@ -7,7 +8,7 @@ class JobOffer(models.Model):
     #location = models.ForeignKey()
     category = models.CharField(max_length=255)
     due_date = models.DateField()
-    start_date = models.DateField()
+    start_date = models.DateField(default=timezone.now())
     description = models.CharField(max_length=255)
     #employer_name = models.ForeignKey()
     #employer_address = models.ForeignKey()
