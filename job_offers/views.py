8d4f52902a0df8b55cd954b5a82004b2ce500cc3
jonasgeirs <jonasgeirs16@gmail.com>
2024-05-07 15:29:32 +0000

    search bar
    working on search, not working

--- job_offers/views.py
+++ job_offers/views.py
@@ -1,9 +1,20 @@
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from job_offers.models import JobOffer
 
 
 def index(request):
+    if 'leit' in request.GET:
+        leit = request.GET['leit']
+        job_offers = [{
+            'title': x.title,
+            'description': x.description,
+            'due date': x.due_date,
+            'percentage': x.percentage
+            #meira
+        } for x in JobOffer.objects.filter(title__icontains=leit)]
+        return JsonResponse({'data': job_offers})
     context = {'job_offers': JobOffer.objects.all().order_by('title')}
     return render(request, 'job_offers_page/index.html', context)
 
