9939d09b2c3a868e9f0fb5f6e5c8e9693aff4ce9
halldora09 <halberjon@gmail.com>
2024-05-08 12:48:17 +0000

    workplace model

--- employers/models.py
+++ employers/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Employer(models.Model):
+class WorkPlace(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()  
     phone = models.CharField(max_length=20)
