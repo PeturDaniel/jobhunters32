499161bec979efbf0f81728fa4e25b091988707f
PeturDaniel <peturdaniel@live.com>
2024-05-09 14:40:12 +0000

    Apply button works

--- job_application/urls.py
+++ job_application/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from job_application.forms import JobExperienceForm, JobReccomendationForm, ApplicationForm
 from job_application.views import JobApplicationWizard
 
+
 urlpatterns = [
     #http://localhost:8000/umsoknir
-    path('job_application_wizard', JobApplicationWizard.as_view([ApplicationForm, JobReccomendationForm, JobExperienceForm]), name='job_application_wizard')
+    path('job_application_wizard', JobApplicationWizard.as_view([ApplicationForm, JobReccomendationForm, JobExperienceForm]), name='job_application_wizard_start')
 ]
 
 '''path('', views.index, name="applications-index"),'''
