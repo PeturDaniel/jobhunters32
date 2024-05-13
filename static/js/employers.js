46d6d8cef254290be42fc7f3cf0977d2d5f8ddd5
jonasgeirs <jonasgeirs16@gmail.com>
2024-05-13 09:58:50 +0000

    search bar tweaks

--- static/js/employers.js
+++ static/js/employers.js
@@ -2,26 +2,37 @@ $(document).ready(function(){
     $('#search-btn').on('click', function(e) {
         e.preventDefault();
         let searchText = $('#search-box').val();
-        $.ajax( {
-            url: '?leit=' + searchText,
-            type: 'GET',
-            success: function(resp){
-                let newHtml = resp.data.map(d => {
-                    return `<div class="well employer">
-                                <a href="/vinnustadir/${d.id}">
-                                    <img class="employer-profile-photo" src="${d.profile_photo}" alt="#"/>
-                                    <h4 class="employer-title">${d.name}</h4>
-                                    <p>${d.address}</p>
-                                </a>
-                            </div>`
-                    });
-                    $('.employers').html(newHtml.join(''));
-                    $('#search-box').val('');
-                },
-            error: function (xhr, status, error) {
-            //finna betri leið, show toastr?
-            console.error(error);
-            }
-        });
+        console.log(searchText.length)
+        if (searchText.trim() !== '')  {
+            $.ajax( {
+                url: '?leit=' + searchText.trim(),
+                type: 'GET',
+                success: function(resp){
+                    let newHtml = resp.data.map(d => {
+                        return `<div class="col">
+                                    <a href="/vinnustadir/${ d.id }" class="card-link">
+                                        <div class="card h-100">
+                                            <img src="${ d.profile_photo }" class="employer-profile-photo rounded mx-auto d-block" alt="...">
+                                            <div class="card-body">
+                                                <h5 class="employer-name">${ d.name }</h5>
+                                                <h6 class="employer-address">${ d.address }</h6>
+                                            </div>
+                                        <div class="card-footer bg-info-subtle">
+                                            <small class="footer-text">blablabla</small>
+                                        </div>
+                                    </a>
+                                </div>`
+                        });
+                        console.log(newHtml)
+                        $('.employers').html(newHtml.join(''));
+                        $('#search-box').val('');
+                    },
+                error: function (xhr, status, error) {
+                //finna betri leið, show toastr?
+                console.error(error);
+                }
+            });
+        }
+        $('#search-box').val('');
     });
 });
--- static/js/job_offers.js
+++ static/js/job_offers.js
@@ -2,27 +2,29 @@ $(document).ready(function(){
     $('#search-btn').on('click', function(e) {
         e.preventDefault();
         let searchText = $('#search-box').val();
-        $.ajax( {
-            url: '?leit=' + searchText,
-            type: 'GET',
-            success: function(resp){
-                let newHtml = resp.data.map(d => {
-                    return `<div class="well_job">
-                                <a href="/lausstorf/${d.id}">
-                                    <img class="employer-profile-photo" src="${d.employer_photo}" alt="#"/>
-                                    <h4 class="job-title">${d.title}</h4>
-                                    <p>Umsóknarfrestur: ${d.due_date}</p>
-                                </a>
-                            </div>`
-                    });
-                    $('.job_offers').html(newHtml.join(''));
-                    $('#search-box').val('');
-                },
-            error: function (xhr, status, error) {
-            //finna betri leið, show toastr?
-            console.error(error);
-            }
-        });
+        if (searchText !== '') {
+            $.ajax( {
+                url: '?leit=' + searchText,
+                type: 'GET',
+                success: function(resp){
+                    let newHtml = resp.data.map(d => {
+                        return `<div class="well_job">
+                                    <a href="/lausstorf/${d.id}">
+                                        <img class="employer-profile-photo" src="${d.employer_photo}" alt="#"/>
+                                        <h4 class="job-title">${d.title}</h4>
+                                        <p>Umsóknarfrestur: ${d.due_date}</p>
+                                    </a>
+                                </div>`
+                        });
+                        $('.job_offers').html(newHtml.join(''));
+                        $('#search-box').val('');
+                    },
+                error: function (xhr, status, error) {
+                //finna betri leið, show toastr?
+                console.error(error);
+                }
+            });
+        }
     });
 });
 
