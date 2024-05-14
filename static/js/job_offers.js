bbf9d4f29816b185ef421535518f19ad6f19719e
gylfi94 <144355995+gylfi94@users.noreply.github.com>
2024-05-14 15:50:57 +0000

    cum

--- static/js/job_offers.js
+++ static/js/job_offers.js
@@ -15,29 +15,8 @@ const doSearchJobs = () => {
     });
 };
 
-
-$(document).ready(function() {
-    $('#companySelect').on('change', function() {
-        var companyName = $(this).val(); // Get the selected company name
-        if (companyName) {
-            $.ajax({
-                url: '/getCompanyData?company=' + encodeURIComponent(companyName),
-                type: 'GET',
-                success: function(resp) {
-                    let newHtml = resp.data.map(d => {
-                        return `<div class="company-info">
-                                    <img src="${d.logo}" alt="Logo of ${d.name}" class="company-logo">
-                                    <h4>${d.name}</h4>
-                                    <p>${d.description}</p>
-                                </div>`;
-                    });
-                    $('#companyData').html(newHtml.join('')); // Assuming companyData is the ID of the container for company info
-                },
-                error: function(xhr, status, error) {
-                    console.error("Error fetching data: ", error);
-                    // Implement error handling, e.g., show a notification or message to the user
-                }
-            });
-        }
-    });
-});
+const filterCompany = () => {
+    const companies = document.querySelectorAll("#companySelect")
+    if ()
+    console.log(companies)
+}
