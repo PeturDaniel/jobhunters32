3377290174af2c483306ae1043cc1e0edeb35912
PeturDaniel <peturdaniel@live.com>
2024-05-14 15:16:53 +0000

    Fixes to job_offers views

--- job_offers/views.py
+++ job_offers/views.py
@@ -3,8 +3,8 @@ from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from job_offers.models import JobOffer
 from django.db.models.functions import Lower
-from job_application.models import Application
 from user.models import JobSeekerProfile
+from job_application.models import Application
 
 
 def index(request):
@@ -21,17 +21,23 @@ def index(request):
 
 
 def get_job_offer_by_id(request, id):
-    profile = JobSeekerProfile.objects.filter(user=request.user).first()
-    enable = True
-    applications = Application.objects.filter(user_id=profile.id)
-    context = {'date': None, 'status': None}
-    for application in applications:
-        if application.job_offer_id == id:
-            enable = False
-            context['date'] = application.sent_date
-            context['status'] = application.status
-    return render(request, 'job_offers_page/job_offer_details.html', {
-        'job_offer': get_object_or_404(JobOffer, pk=id),
-        'enable': enable,
-        'context': context
-    })
+    if request.user.is_authenticated:
+        profile = JobSeekerProfile.objects.filter(user=request.user).first()
+        applications = Application.objects.filter(user_id=profile.id)
+        enable = True
+        context = {'date': None, 'status': None}
+        for application in applications:
+            if application.job_offer_id == id:
+                enable = False
+                context['date'] = application.sent_date
+                context['status'] = application.status
+        return render(request, 'job_offers_page/job_offer_details.html', {
+            'job_offer': get_object_or_404(JobOffer, pk=id),
+            'enable': enable,
+            'context': context
+        })
+    else:
+        return render(request, 'job_offers_page/job_offer_details.html', {
+            'job_offer': get_object_or_404(JobOffer, pk=id),
+            'enable': True
+        })
