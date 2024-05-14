4385e1c25050bd711325872a1bd68c897bfc538c
PeturDaniel <peturdaniel@live.com>
2024-05-14 11:51:51 +0000

    Status of application displayed on already applied jobs

--- job_offers/views.py
+++ job_offers/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from job_offers.models import JobOffer
+from job_application.models import Application
+from user.models import JobSeekerProfile
 
 
 def index(request):
@@ -22,6 +24,17 @@ def index(request):
 
 
 def get_job_offer_by_id(request, id):
+    profile = JobSeekerProfile.objects.filter(user=request.user).first()
+    enable = True
+    applications = Application.objects.filter(user_id=profile.id)
+    context = {'date': None, 'status': None}
+    for application in applications:
+        if application.job_offer_id == id:
+            enable = False
+            context['date'] = application.sent_date
+            context['status'] = application.status
     return render(request, 'job_offers_page/job_offer_details.html', {
-        'job_offer': get_object_or_404(JobOffer, pk=id)
+        'job_offer': get_object_or_404(JobOffer, pk=id),
+        'enable': enable,
+        'context': context
     })
